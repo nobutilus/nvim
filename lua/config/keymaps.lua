053b972c02652838742c902d27c7c6eafe0d592a
Nobuya Mizoguchi <n_mizoguchi@yumemi.co.jp>
2025-10-26 22:45:54 +0900

    update

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -8,24 +8,3 @@ end, { desc = "Compare 2 files" })
 vim.keymap.set("n", "<leader>sc", function()
   require("telescope").extensions.diff.diff_current({ hidden = true })
 end, { desc = "Compare file with current" })
-
--- CopilotChat keymaps
-local function map(mode, lhs, rhs, opts)
-  local options = { noremap = true, silent = true }
-  if opts then
-    options = vim.tbl_extend("force", options, opts)
-  end
-  vim.keymap.set(mode, lhs, rhs, options)
-end
-
--- Copilot Chat
-map("n", "<leader>cc", ":CopilotChat<CR>", { desc = "Copilot Chat" })
-map("n", "<leader>cfd", ":CopilotChatFixDiagnostic<CR>", { desc = "Fix diagnostic" })
-map("n", "<leader>cgm", ":CopilotChatCommit<CR>", { desc = "Generate commit" })
-map("n", "<leader>cgs", ":CopilotChatCommitStaged<CR>", { desc = "Generate commit staged" })
-map("v", "<leader>ce", ":CopilotChatExplain<CR>", { desc = "Explain code" })
-map("v", "<leader>cf", ":CopilotChatFix<CR>", { desc = "Fix code" })
-map("v", "<leader>co", ":CopilotChatOptimize<CR>", { desc = "Optimize code" })
-map("v", "<leader>cd", ":CopilotChatDocs<CR>", { desc = "Add docs" })
-map("v", "<leader>ct", ":CopilotChatTests<CR>", { desc = "Generate tests" })
-map("v", "<leader>cc", ":CopilotChat<CR>", { desc = "Copilot Chat with selection" })
